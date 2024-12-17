ad2c3176d904fbd149877e479ecb8885c12a4b98
HomePC <1553170687@qq.com>
2024-12-17 19:47:41 +0800

    [add]添加release和debug的对应配置文件

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,11 @@
 // #include "c_11.hpp"
 #include <iostream>
+#include <thread>
+#include <chrono>
 int main() {
     std::cout << "Hello, world!" << std::endl;
     // c11_example();
+    //暂停5s
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
 }
