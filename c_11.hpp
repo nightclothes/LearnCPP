8005fe739649203218857a18b4723c811bf3e962
HomePC <1553170687@qq.com>
2024-12-17 14:41:49 +0800

    [init]初始化

---
+++ c_11.hpp
@@ -0,0 +1,8 @@
+// c11标准 特性举例
+#pragma once
+#include <iostream>
+void printff();
+void printff() {
+    std::cout << "Hello, world from c_11.cpp" << std::endl;
+    return;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "c_11.hpp"
+#include <iostream>
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    printff();
+    return 0;
+}
