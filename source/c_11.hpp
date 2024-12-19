a4e8fb7cf2f7b3c6dc43094e9137e6b120c52f35
WorkPC#1 <1553170687@qq.com>
2024-12-19 10:16:18 +0800

    test pull

--- source/c_11.hpp
+++ source/c_11.hpp
@@ -3,15 +3,6 @@
 #include <iostream>
 
 void c11_example() {
-    auto x = 10;
-    std::cout <<"type of x is "<< typeid(x).name() << std::endl;
-
-    auto sum = [](int a, int b) {
-        return a + b;
-    };
-    std::cout << sum(1, 2) << std::endl;
-
-    auto str = R"(This is a s\tring)";
-    std::cout << str << std::endl;
+    std::cout << "c11_example" << std::endl;
 
 }
