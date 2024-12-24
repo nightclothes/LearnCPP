20181bf38b93a95e00d8862c5895c714665352f7
HomePC <1553170687@qq.com>
2024-12-24 23:58:04 +0800

    [add]添加imgui(dx11 win32)

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,12 @@
 #include "test_c11.hpp"
 #include "test_spdlog.hpp"
+#include "test_imgui"
 #include <iostream>
 #include <thread>
 #include <chrono>
 int main() {
     std::cout << "Hello, world!" << std::endl;
-    spdlog_example();
+    imgui_example();
     //暂停5s
     std::this_thread::sleep_for(std::chrono::seconds(5));
     return 0;
